1603819f67ec1e18f9d29d75ce364be672b3a35d
ChloeArd <chloe.ardoise@gmail.com>
2021-02-04 15:04:08 +0100

    Exercice 176

--- index.php
+++ index.php
@@ -3,3 +3,31 @@ $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
 echo "Ce dictionnaire contient ".$nb = count( $dico )." mots. <br>" ;
+
+$cara15 = 0;
+foreach ($dico as $word) {
+    if (strlen($word) === 15) {
+        $cara15++;
+    }
+}
+echo "Il y a ".$cara15." mots qui font exactement 15 caractères <br>";
+
+$withW = 0;
+foreach ($dico as $word) {
+    if (stripos($word, "w")) {
+        $withW++;
+    }
+}
+echo "Il y a ".$withW." mots qui contiennent la lettre W <br>";
+
+
+$endQ = 0;
+foreach ($dico as $word) {
+    if ($word[-2] === 'q') {
+        $endQ++;
+    }
+}
+
+echo "Il y a ".$endQ." mots qui finissent par la lettre Q <br>";
+
+
